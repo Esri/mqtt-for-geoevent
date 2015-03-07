16509b6789f17443cfe1a046f16eda30a7b8c4c1
Hanoch Kalmanovich <hkalmanovich@esri.com>
2015-03-06 17:23:06 -0800

    initial repo commit

---
+++ mqtt-transport/src/main/java/com/esri/geoevent/transport/mqtt/MqttInboundTransport.java
@@ -0,0 +1,225 @@
+/*
+  Copyright 1995-2015 Esri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+  For additional information, contact:
+  Environmental Systems Research Institute, Inc.
+  Attn: Contracts Dept
+  380 New York Street
+  Redlands, California, USA 92373
+
+  email: contracts@esri.com
+*/
+
+package com.esri.geoevent.transport.mqtt;
+
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import com.esri.ges.core.component.ComponentException;
+import com.esri.ges.core.component.RunningException;
+import com.esri.ges.core.component.RunningState;
+import com.esri.ges.framework.i18n.BundleLogger;
+import com.esri.ges.framework.i18n.BundleLoggerFactory;
+import com.esri.ges.transport.InboundTransportBase;
+import com.esri.ges.transport.TransportDefinition;
+
+public class MqttInboundTransport extends InboundTransportBase implements Runnable
+{
+
+	private static final BundleLogger	log			= BundleLoggerFactory.getLogger(MqttInboundTransport.class);
+
+	private Thread										thread	= null;
+	private int												port;
+	private String										host;
+	private String										topic;
+	private MqttClient								mqttClient;
+
+	public MqttInboundTransport(TransportDefinition definition) throws ComponentException
+	{
+		super(definition);
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	public void start() throws RunningException
+	{
+		try
+		{
+			switch (getRunningState())
+			{
+				case STARTING:
+				case STARTED:
+				case STOPPING:
+					return;
+			}
+			setRunningState(RunningState.STARTING);
+			thread = new Thread(this);
+			thread.start();
+		}
+		catch (Exception e)
+		{
+			log.error("UNEXPECTED_ERROR_STARTING", e);
+			stop();
+		}
+	}
+
+	@Override
+	public void run()
+	{
+		this.receiveData();
+	}
+
+	private void receiveData()
+	{
+		try
+		{
+			applyProperties();
+			setRunningState(RunningState.STARTED);
+
+			String url = "tcp://" + host + ":" + Integer.toString(port);
+			mqttClient = new MqttClient(url, MqttClient.generateClientId(), new MemoryPersistence());
+
+			mqttClient.setCallback(new MqttCallback()
+				{
+
+					@Override
+					public void messageArrived(String topic, MqttMessage message) throws Exception
+					{
+						try
+						{
+							receive(message.getPayload());
+						}
+						catch (RuntimeException e)
+						{
+							e.printStackTrace();
+						}
+					}
+
+					@Override
+					public void deliveryComplete(IMqttDeliveryToken token)
+					{
+						// not used
+					}
+
+					@Override
+					public void connectionLost(Throwable cause)
+					{
+						log.error("CONNECTION_LOST", cause.getLocalizedMessage());
+					}
+				});
+			mqttClient.connect();
+			mqttClient.subscribe(topic, 1);
+
+		}
+		catch (Throwable ex)
+		{
+			log.error("UNEXPECTED_ERROR", ex);
+			setRunningState(RunningState.ERROR);
+		}
+	}
+
+	private void receive(byte[] bytes)
+	{
+		if (bytes != null && bytes.length > 0)
+		{
+			String str = new String(bytes);
+			str = str + '\n';
+			byte[] newBytes = str.getBytes();
+
+			ByteBuffer bb = ByteBuffer.allocate(newBytes.length);
+			try
+			{
+				bb.put(newBytes);
+				bb.flip();
+				byteListener.receive(bb, "");
+				bb.clear();
+			}
+			catch (BufferOverflowException boe)
+			{
+				log.error("BUFFER_OVERFLOW_ERROR", boe);
+				bb.clear();
+				setRunningState(RunningState.ERROR);
+			}
+			catch (Exception e)
+			{
+				log.error("UNEXPECTED_ERROR2", e);
+				stop();
+				setRunningState(RunningState.ERROR);
+			}
+		}
+	}
+
+	private void applyProperties() throws Exception
+	{
+		port = 1883; // default
+		if (getProperty("port").isValid())
+		{
+			int value = (Integer) getProperty("port").getValue();
+			if (value > 0 && value != port)
+			{
+				port = value;
+			}
+		}
+
+		host = "iot.eclipse.org"; // default
+		if (getProperty("host").isValid())
+		{
+			String value = (String) getProperty("host").getValue();
+			if (!value.trim().equals(""))
+			{
+				host = value;
+			}
+		}
+
+		topic = "topic/sensor"; // default
+		if (getProperty("topic").isValid())
+		{
+			String value = (String) getProperty("topic").getValue();
+			if (!value.trim().equals(""))
+			{
+				topic = value;
+			}
+		}
+	}
+
+	public synchronized void stop()
+	{
+		try
+		{
+			if (this.mqttClient != null)
+			{
+				this.mqttClient.disconnect();
+				this.mqttClient.close();
+			}
+		}
+		catch (MqttException ex)
+		{
+			log.error("UNABLE_TO_CLOSE", ex);
+		}
+		setRunningState(RunningState.STOPPED);
+	}
+
+	@Override
+	public boolean isClusterable()
+	{
+		return false;
+	}
+}
---
+++ mqtt-transport/src/main/java/com/esri/geoevent/transport/mqtt/MqttOutboundTransport.java
@@ -0,0 +1,166 @@
+/*
+  Copyright 1995-2015 Esri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+  For additional information, contact:
+  Environmental Systems Research Institute, Inc.
+  Attn: Contracts Dept
+  380 New York Street
+  Redlands, California, USA 92373
+
+  email: contracts@esri.com
+*/
+
+package com.esri.geoevent.transport.mqtt;
+
+import java.nio.ByteBuffer;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import com.esri.ges.core.component.ComponentException;
+import com.esri.ges.core.component.RunningException;
+import com.esri.ges.core.component.RunningState;
+import com.esri.ges.framework.i18n.BundleLogger;
+import com.esri.ges.framework.i18n.BundleLoggerFactory;
+import com.esri.ges.transport.OutboundTransportBase;
+import com.esri.ges.transport.TransportDefinition;
+
+public class MqttOutboundTransport extends OutboundTransportBase
+{
+
+	private static final BundleLogger	log	= BundleLoggerFactory.getLogger(MqttOutboundTransport.class);
+
+	private int												port;
+	private String										host;
+	private String										topic;
+	private MqttClient								mqttClient;
+
+	public MqttOutboundTransport(TransportDefinition definition) throws ComponentException
+	{
+		super(definition);
+	}
+
+	@Override
+	public void start() throws RunningException
+	{
+		try
+		{
+			setRunningState(RunningState.STARTING);
+			applyProperties();
+			connectMqtt();
+			setRunningState(RunningState.STARTED);
+		}
+		catch (Exception e)
+		{
+			log.error("INIT_ERROR", e, this.getClass().getName());
+			setRunningState(RunningState.ERROR);
+		}
+	}
+
+	@Override
+	public void receive(ByteBuffer buffer, String channelId)
+	{
+		try
+		{
+			if (mqttClient == null || !mqttClient.isConnected())
+				connectMqtt();
+
+			byte[] b = new byte[buffer.remaining()];
+			buffer.get(b);
+
+			mqttClient.publish(topic, b, 2, true);
+		}
+		catch (Exception e)
+		{
+			log.error("ERROR_PUBLISHING", e);
+		}
+	}
+
+	private void connectMqtt() throws MqttException
+	{
+		String url = "tcp://" + host + ":" + Integer.toString(port);
+		mqttClient = new MqttClient(url, MqttClient.generateClientId(), new MemoryPersistence());
+		mqttClient.connect();
+	}
+
+	private void applyProperties() throws Exception
+	{
+		port = 1883; // default
+		if (getProperty("port").isValid())
+		{
+			int value = (Integer) getProperty("port").getValue();
+			if (value > 0 && value != port)
+			{
+				port = value;
+			}
+		}
+
+		host = "iot.eclipse.org"; // default
+		if (getProperty("host").isValid())
+		{
+			String value = (String) getProperty("host").getValue();
+			if (!value.trim().equals(""))
+			{
+				host = value;
+			}
+		}
+
+		topic = "topic/actuators/light"; // default
+		if (getProperty("topic").isValid())
+		{
+			String value = (String) getProperty("topic").getValue();
+			if (!value.trim().equals(""))
+			{
+				topic = value;
+			}
+		}
+	}
+
+	@Override
+	public synchronized void stop()
+	{
+		setRunningState(RunningState.STOPPING);
+
+		if (this.mqttClient != null)
+			disconnectMqtt();
+
+		setRunningState(RunningState.STOPPED);
+	}
+
+	private void disconnectMqtt()
+	{
+		try
+		{
+			if (mqttClient != null)
+			{
+				if (mqttClient.isConnected())
+				{
+					mqttClient.disconnect();
+					mqttClient.close();
+				}
+			}
+		}
+		catch (MqttException e)
+		{
+			log.error("UNABLE_TO_CLOSE", e);
+		}
+		finally
+		{
+			mqttClient = null;
+		}
+	}
+
+}
